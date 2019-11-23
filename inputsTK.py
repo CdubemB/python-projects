7f92a572e644c5da17b5d66ea64cf62e10304a31
CdubemB <dubemwb@gmail.com>
2019-11-23 15:38:18 +0000

    fixed spelling errors ; added comments

--- inputsTK.py
+++ inputsTK.py
@@ -3,12 +3,14 @@ from tkinter import * #imports the tkinter module
 
 base = Tk()
 
-e = Entry(base, width=50, fg="black", borderwidth=2) #creats a search bar
+e = Entry(base, width=50, fg="black", borderwidth=2) #creates a search bar
+e.insert(0, "Enter your name:")# e.insert gives presest text in the box (0 represents the index)
 e.pack()
 
 def uClick():
-   uLabel = Label(base, text=e.get())
-   uLabel.pack() #creats a string of text
+   greeting = "good morrow " + e.get() #.get() gets the text inside of the Entry/text bar
+   uLabel = Label(base, text=greeting)
+   uLabel.pack() #creates a string of text
 
 uButton = Button(base, text="Enter name user NO.18", command=uClick)
 uButton.pack() #creates a button which will run a function
