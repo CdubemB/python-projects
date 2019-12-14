eb2471521a4b9757658f8fd163b28cd933e130d9
CdubemB <dubemwb@gmail.com>
2019-12-14 15:28:41 +0000

    have changed the strings

--- inputsTK.py
+++ inputsTK.py
@@ -4,7 +4,7 @@ from tkinter import * #imports the tkinter module
 base = Tk()
 
 e = Entry(base, width=50, fg="black", borderwidth=2) #creates a search bar
-e.insert(0, "Enter your name:")# e.insert gives presest text in the box (0 represents the index)
+e.insert(0, "Please Enter your name:")# e.insert gives presest text in the box (0 represents the index)
 e.pack()
 
 def uClick():
@@ -12,7 +12,7 @@ def uClick():
    uLabel = Label(base, text=greeting)
    uLabel.pack() #creates a string of text
 
-uButton = Button(base, text="Enter name user NO.18", command=uClick)
+uButton = Button(base, text="Please Enter name user NO.18", command=uClick)
 uButton.pack() #creates a button which will run a function.
 
 base.mainloop()
