debc87ac58b9b4616cbd3e4c66a5326a353103c8
CdubemB <dubemwb@gmail.com>
2019-12-15 14:34:52 +0000

    changing a string in the code

---
+++ TKcalc.py
@@ -0,0 +1,18 @@
+#creating a calculator on python
+from tkinter import * #imports the tkinter module
+
+base = Tk()
+
+tBar = Entry(base, width=50, fg="black", borderwidth=2) #creates a search bar
+tBar.insert(0, "Please Enter your name:")# e.insert gives presest text in the box (0 represents the index)
+tBar.pack()
+
+def uClick():
+   greeting = "good morrow Dr" + tBar.get() #.get() gets the text inside of the Entry/text bar
+   uLabel = Label(base, text=greeting)
+   uLabel.pack() #creates a string of text
+
+uButton = Button(base, text="Please Enter name user NO.18", command=uClick)
+uButton.pack() #creates a button which will run a function.
+
+base.mainloop()
