966f5b87a3996eec10754780f2616e5485a5651f
CdubemB <dubemwb@gmail.com>
2020-01-06 21:06:34 +0000

    have created buttons for the calculator

--- TKcalc.py
+++ TKcalc.py
@@ -3,16 +3,42 @@ from tkinter import * #imports the tkinter module
 
 base = Tk()
 
-tBar = Entry(base, width=50, fg="black", borderwidth=2) #creates a search bar
-tBar.insert(0, "Please Enter your name:")# e.insert gives presest text in the box (0 represents the index)
-tBar.pack()
+tBar = Entry(base, width=35, fg="black", borderwidth=5) #creates a search bar
+tBar.grid(row = 0, column = 0, columnspan = 3, padx = 10, pady = 10)
+def button_click(number):
+    tBar.delete(0, END)
+    tBar.insert(0, number)
 
-def uClick():
-   greeting = "good morrow Dr" + tBar.get() #.get() gets the text inside of the Entry/text bar
-   uLabel = Label(base, text=greeting)
-   uLabel.pack() #creates a string of text
+#this will define the buttons
 
-uButton = Button(base, text="Please Enter name user NO.18", command=uClick)
-uButton.pack() #creates a button which will run a function.
+button_1 = Button(base, text="1", pady=20, padx=40, command=lambda: button_click(1))
+button_2 = Button(base, text="2", pady=20, padx=40, command=lambda: button_click(2))
+button_3 = Button(base, text="3", pady=20, padx=40, command=lambda: button_click(3))
+button_4 = Button(base, text="4", pady=20, padx=40, command=lambda: button_click(4))
+button_5 = Button(base, text="5", pady=20, padx=40, command=lambda: button_click(5))
+button_6 = Button(base, text="6", pady=20, padx=40, command=lambda: button_click(6))
+button_7 = Button(base, text="7", pady=20, padx=40, command=lambda: button_click(7))
+button_8 = Button(base, text="8", pady=20, padx=40, command=lambda: button_click(8))
+button_9 = Button(base, text="9", pady=20, padx=40, command=lambda: button_click(9))
+button_0 = Button(base, text="0", pady=20, padx=40, command=lambda: button_click(0))
+button_add = Button(base, text="+", pady=20, padx=39, command=lambda: button_click())
+button_equal = Button(base, text="=", pady=20, padx=91, command=lambda: button_click())
+button_clear = Button(base, text="clear", pady=20, padx=79, command=lambda: button_click())
 
+button_1.grid(row=1, column=0)
+button_2.grid(row=1, column=1)
+button_3.grid(row=1, column=2)
+
+button_4.grid(row=2, column=0)
+button_5.grid(row=2, column=1)
+button_6.grid(row=2, column=2)
+
+button_7.grid(row=3, column=0)
+button_8.grid(row=3, column=1)
+button_9.grid(row=3, column=2)
+
+button_0.grid(row=4, column=0)
+button_add.grid(row=5, column=0)
+button_equal.grid(row=5, column=1, columnspan=2)# due to it spanning two lines
+button_clear.grid(row=4, column=1, columnspan=2)# due to it spanning two lines
 base.mainloop()
