79327b3f521db1fe649299fa6cca06d42c24721c
CdubemB <dubemwb@gmail.com>
2020-01-15 20:31:03 +0000

    fixed the if statement

--- simpleCalc.py
+++ simpleCalc.py
@@ -7,13 +7,13 @@ y = int(input("what is the first number you wish to use \n"))
 x = int(input("what is the second number you wish to use \n"))
 #the numbers that will be added
 def uCalc(num1, num2):
-	if function == / or function == "division":
+	if function == "/" or function == "division":
 		print(num1 / num2)
-	elif function == * or function == "multiplication":
+	elif function == "*" or function == "multiplication":
 		print(num1 * num2)
-	if function == + or function == "addition":
+	if function == "+" or function == "addition":
 		print(num1 + num2)
-	if function == - or function == "subtraction":
+	if function == "-" or function == "subtraction":
 		print(num1 - num2)
 
 #Function carries out the equation
