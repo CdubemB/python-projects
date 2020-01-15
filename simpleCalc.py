0b1150cd4a5e67db20332f25e2bf822d5e7511d6
CdubemB <dubemwb@gmail.com>
2020-01-15 20:26:10 +0000

    Added comments to the code

--- simpleCalc.py
+++ simpleCalc.py
@@ -2,10 +2,10 @@
 	Creating a simple calculator
 """
 function = str(input("what function do you need to use (addition, subtraction, multiplication or division) \n"))
-
+#acuries the function from the user
 y = int(input("what is the first number you wish to use \n"))
 x = int(input("what is the second number you wish to use \n"))
-
+#the numbers that will be added
 def uCalc(num1, num2):
 	if function == / or function == "division":
 		print(num1 / num2)
@@ -16,5 +16,5 @@ def uCalc(num1, num2):
 	if function == - or function == "subtraction":
 		print(num1 - num2)
 
-
+#Function carries out the equation
 uCalc(x, y)
