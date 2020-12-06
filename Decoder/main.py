cbdbe97eb9dbc374e2866433b882c7888f1ef59c
CdubemB <dubemwb@gmail.com>
2020-12-06 17:43:57 +0000

    created file with overview of the project

---
+++ Decoder/main.py
@@ -0,0 +1,12 @@
+#WRITTEN BY CdubemB
+
+"""
+create a programme that can:
+
+gather a sample text with variables
+gather variable defintions from the user
+decode the text
+
+a tkinter user interface is adviced
+"""
+
