068f2c0a336e9c1865f3f878872380ae7265f3e0
CdubemB <dubemwb@gmail.com>
2020-12-06 17:55:47 +0000

    created functions (with comments)

--- Decoder/main.py
+++ Decoder/main.py
@@ -9,4 +9,18 @@ decode the text
 
 a tkinter user interface is adviced
 """
+from tkinter import *
 
+def main():#will run all the following functions
+    text_gather()
+    translate()
+    result()
+
+def text_gather():#will get the text input with the variables from the user
+    pass
+    
+def translate():#will translate the given variables that the user inserts
+    pass
+
+def result():#will output the translated text
+    pass
