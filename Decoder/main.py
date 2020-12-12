ed98810027d4201795e7d9a36659d844b91afeed
CdubemB <dubemwb@gmail.com>
2020-12-12 10:42:20 +0000

    made quick edit

--- Decoder/main.py
+++ Decoder/main.py
@@ -9,18 +9,26 @@ decode the text
 
 a tkinter user interface is adviced
 """
-from tkinter import *
+from tkinter import 
+
+base =  Tk()
 
 def main():#will run all the following functions
     text_gather()
     translate()
     result()
+    base.mainloop()
 
 def text_gather():#will get the text input with the variables from the user
-    pass
+	uLabel = Label(base, text="enter your paragraph with the variables")
+	uLabel.pack()
+    tBar = Entry(base, width=35, fg="black", borderwidth=5) #creates a text bar for the box
+    tBar.grid(row = 0, column = 0, columnspan = 3, padx = 10, pady = 50) #puts the text bar in a specific place
     
 def translate():#will translate the given variables that the user inserts
     pass
 
 def result():#will output the translated text
     pass
+
+main()
