8fc8e74e5bf20a296441713d26f352838eef2e08
CdubemB <dubemwb@gmail.com>
2025-09-01 14:21:35 +0100

    checked sync

--- base_python/useful_functions.py
+++ base_python/useful_functions.py
@@ -33,7 +33,7 @@ else:
     print("not all of the numbers a greater than five")
 
 if any(i > 5 for i in numbers):
-    print("at least one is greater than five")#checks if any numbers are greater than five
+    print("at least one is greater than five")#checks if any numbers are greater than five.
 
 for e in enumerate(numbers):
     print(e) # prints all the numbers in the list in their order with given iteration.
