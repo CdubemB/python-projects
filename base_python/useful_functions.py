a1eb73e2b781d3513e02ff3f5370e4fbda1fcfc6
CdubemB <dubemwb@gmail.com>
2025-09-01 14:03:36 +0100

    Added to more code to useful_function on python and added to Javascript

--- base_python/useful_functions.py
+++ base_python/useful_functions.py
@@ -1,6 +1,31 @@
 # USEFUL FUNCTIONS IN PYTHON BY CdubemB
+ingredients = ["cheese", "egg", "ham", "siracha"]
+print(", ".join(ingredients)) #the join function will print the list with the , as a seperator
+ingredients2 = (", ".join(ingredients)).split(", ") #the split function converts a string wiht a certain seperator into a list
+print(ingredients2)
 
-numbers = [1, 3, 5, 7, 9]#list of odd numbers
+print ("Bonjour Shofu".replace("shofu", "le monde"))#this will replace shofu with le monde
+
+sentence = "Susan went to Mexico"
+if sentence.startswith("Susan") and sentence.endswith("Mexico"):
+    print("Susan truly did go to Mexico")
+else:
+    print("Susan may have not went to Mexico")
+#the startswith and endswith function check if a sentence begins, or ends with a word respectively
+ 
+print(sentence.lower())
+print(sentence.upper())
+# the .upper() function converts a sentence to capital letters whilst the .lower() does the inverse
+
+numbers = [1, 3, 5, 7, 9]
+
+print(min(numbers))
+print(max(numbers))
+print(sum(numbers))
+#the min function produces the minimum value in a list, the max function produces the max and the sum function gives the sum of the list
+
+print(abs(-44))
+#the abs function gives the absolute function of a number (distance from zero)
 
 if all(i > 1 for i in numbers):
     print("all of the numbers are greater than five") # checks for if all numbers are greater than five
