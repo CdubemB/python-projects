3e1a9a1357c141cdc659ce2920e157e572777e0a
CdubemB <dubemwb@gmail.com>
2025-09-05 12:30:38 +0100

    added a simple fibonacci

--- base_python/functions.py
+++ base_python/functions.py
@@ -41,3 +41,11 @@ def factorial(x):
 print(factorial(5))
 
 #the above is an example of a recursive function (a function that refers to itself)
+
+def fib(x):
+      if x == 0 or x == 1:
+         return 1
+      else:
+         return fib(x-1) + fib(x-2)
+      
+print(fib(4))
