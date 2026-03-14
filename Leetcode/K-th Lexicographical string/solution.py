047193c7dcc289db49170a45796d551b97e29392
CdubemB <williamcbardi@gmail.com>
2026-03-14 10:26:11 +0000

    ...

--- Leetcode/K-th Lexicographical string/solution.py
+++ Leetcode/K-th Lexicographical string/solution.py
@@ -1,3 +1,3 @@
 class solution:
-    def getHappyString(self, n: int, k: int) -> str:
+    def getHappyString(self, n: int, k: int) -> str: #this object will be used to determine whether input is a possible happystring
         
